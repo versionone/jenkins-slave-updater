211952b0f65f92606f60357d7efac1318f544cc6
Steven Harman <steveharman@gmail.com>
2011-05-19 16:19:17 -0400

    invoke-command for native stuff. oops!

--- jenkins-slave-updater.ps1
+++ jenkins-slave-updater.ps1
@@ -1,4 +1,4 @@
-# version 0.1.0
+# version 0.2.0
 # for more info, see: https://github.com/versionone/jenkins-slave-updater
 
 echo "=== Stopping Jenkins Slave ==="
@@ -7,9 +7,7 @@ $jenkins.Stop()
 $jenkins.WaitForStatus("Stopped")
 
 echo "=== Getting new slave.jar from Master server ==="
-$updater = start-job -name JenkinsSlaveUpdater { curl -O http://ci-server.corp.versionone.net/jnlpJars/slave.jar }
-wait-job $updater
-receive-job -j $updater
+invoke-command -scriptblock { curl -O "http://ci-server.corp.versionone.net/jnlpJars/slave.jar" }
 
 echo "=== Starting Jenkins Slave ==="
 $jenkins.Start()
